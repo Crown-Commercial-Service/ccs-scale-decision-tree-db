e04d9efde3a8030cfa76dd614b79e53fb1901887
tommyb82 <thomasbunting@gmail.com>
2020-06-19 13:59:24 +0100

    Set NEO4J_AUTH from env

--- wrapper.sh
+++ wrapper.sh
@@ -3,8 +3,10 @@ set -meo pipefail
 
 function load_gm_data() {
   echo "Loading Guided Match decision tree data from file: ${1}"
-  cypher-shell -u neo4j -p sbx_graph -a bolt://localhost:7687 --debug -f ./graph/$1
+  cypher-shell -u ${DB_ADMIN_USERNAME} -p "${DB_ADMIN_PASSWORD}" -a bolt://localhost:7687 --debug -f ./graph/$1
 }
+
+export NEO4J_AUTH="${DB_ADMIN_USERNAME}/${DB_ADMIN_PASSWORD}"
 
 # Start the primary process in the background
 /docker-entrypoint.sh neo4j &
@@ -27,8 +29,13 @@ load_gm_data "common.cypher"
 load_gm_data "RM6154_Linen_and_Laundry.cypher"
 load_gm_data "RM3788_Legal_Services.cypher"
 load_gm_data "RM3733_RM6068_RM6103_TP2_TePAS_EduTech.cypher"
-#load_gm_data "MCF2_Consultancy.cypher"
+load_gm_data "MCF2_Consultancy.cypher"
 
 echo "Data load complete"
 
+# Create a read-only service account:
+createUserCypher="CREATE USER ${DB_READ_ONLY_USERNAME} IF NOT EXISTS SET PASSWORD '${DB_READ_ONLY_PASSWORD}' SET PASSWORD CHANGE NOT REQUIRED;"
+grantReaderRoleCypher="GRANT ROLE reader TO ${DB_READ_ONLY_USERNAME};"
+cypher-shell -u ${DB_ADMIN_USERNAME} -p ${DB_ADMIN_PASSWORD} -a bolt://localhost:7687 -d system --debug "${createUserCypher}"
+#cypher-shell -u ${DB_ADMIN_USERNAME} -p ${DB_ADMIN_PASSWORD} -a bolt://localhost:7687 -d system --debug "${grantReaderRoleCypher}"
 fg %1

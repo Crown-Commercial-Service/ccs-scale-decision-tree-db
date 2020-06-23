fbc84d923a660b7d1d72c06d655047c3381c94a0
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-06-23 10:40:46 +0100

    Feature/sfat 153 decision tree db params (#23)
    * Set NEO4J_AUTH from env
    
    * Update remove refs to read-only account

--- wrapper.sh
+++ wrapper.sh
@@ -3,8 +3,10 @@ set -meo pipefail
 
 function load_gm_data() {
   echo "Loading Guided Match decision tree data from file: ${1}"
-  cypher-shell -u neo4j -p sbx_graph -a bolt://localhost:7687 --debug -f ./graph/$1
+  cypher-shell -u ${DB_ADMIN_USERNAME} -p "${DB_ADMIN_PASSWORD}" -a bolt://localhost:7687 --debug -f ./graph/$1
 }
+
+export NEO4J_AUTH="${DB_ADMIN_USERNAME}/${DB_ADMIN_PASSWORD}"
 
 # Start the primary process in the background
 /docker-entrypoint.sh neo4j &
@@ -27,8 +29,11 @@ load_gm_data "common.cypher"
 load_gm_data "RM6154_Linen_and_Laundry.cypher"
 load_gm_data "RM3788_Legal_Services.cypher"
 load_gm_data "RM3733_RM6068_RM6103_TP2_TePAS_EduTech.cypher"
-#load_gm_data "MCF2_Consultancy.cypher"
+load_gm_data "MCF2_Consultancy.cypher"
 
 echo "Data load complete"
 
+# Create a service account (for use by Decision Tree service):
+createUserCypher="CREATE USER ${DB_SERVICE_ACCOUNT_USERNAME} IF NOT EXISTS SET PASSWORD '${DB_SERVICE_ACCOUNT_PASSWORD}' SET PASSWORD CHANGE NOT REQUIRED;"
+cypher-shell -u ${DB_ADMIN_USERNAME} -p ${DB_ADMIN_PASSWORD} -a bolt://localhost:7687 -d system --debug "${createUserCypher}"
 fg %1

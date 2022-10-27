42e0bd7111cb82a00d71a478aec06db26823bdd2
dorota-hawro <83641223+dorota-hawro@users.noreply.github.com>
2022-10-27 13:54:20 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -49,6 +49,7 @@ load_gm_data "Staffing_Solutions.cypher"
 #load_gm_data "MCF2_Consultancy.cypher"
 load_gm_data "RM6119_Furniture_and_Associated_Services.cypher"
 load_gm_data "RM6133_Employee_Benefits.cypher"
+load_gm_data "RM6011_Supply_Energy_Ancillary_Services.cypher"
 load_gm_data "RM6141_Language_Services.cypher"
 
 echo "Data load complete"
